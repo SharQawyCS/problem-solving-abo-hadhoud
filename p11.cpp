09e7e7c1bb9454994c96b956f849d9c32aa071cb
SharQawyCS <sharqawycs@gmail.com>
2024-06-21 20:47:27 +0300

    #

--- p11.cpp
+++ p11.cpp
@@ -17,30 +17,37 @@ void printMatrix(int arr[3][3], short Rows, short Cols)
 
 bool isMatricesEqual(int matrix1[3][3], int matrix2[3][3], short rows, short cols)
 {
+  int sum1 = 0, sum2 = 0;
   for (int i = 0; i < rows; ++i)
   {
     for (int j = 0; j < cols; ++j)
     {
-      if (matrix1[i][j] != matrix2[i][j])
-      {
-        return false;
-      }
+      sum1 += matrix1[i][j];
+      sum2 += matrix2[i][j];
     }
   }
-  return true;
+
+  if (sum1 == sum2)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 int main()
 {
   int mat1[3][3] =
       {
-          {1, 2, 3},
+          {0, 9, 3},
           {6, 7, 7},
           {7, 8, 8}},
 
       mat2[3][3] =
           {
-              {1, 2, 8},
+              {1, 2, 9},
               {6, 7, 7},
               {7, 8, 8}};
   printMatrix(mat1, 3, 3);
