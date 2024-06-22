c80b6ccfe538203c76c913326e75bc325aa0b550
SharQawyCS <sharqawycs@gmail.com>
2024-06-22 06:45:32 +0300

    #

--- library.cpp
+++ library.cpp
@@ -286,6 +286,43 @@ void printMatrix(int arr[3][3], short Rows, short Cols)
   }
 }
 
+bool isMatricesTypical(int matrix1[3][3], int matrix2[3][3], short rows, short cols)
+{
+  for (int i = 0; i < rows; ++i)
+  {
+    for (int j = 0; j < cols; ++j)
+    {
+      if (matrix1[i][j] != matrix2[i][j])
+      {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+bool isMatricesEqual(int matrix1[3][3], int matrix2[3][3], short rows, short cols)
+{
+  int sum1 = 0, sum2 = 0;
+  for (int i = 0; i < rows; ++i)
+  {
+    for (int j = 0; j < cols; ++j)
+    {
+      sum1 += matrix1[i][j];
+      sum2 += matrix2[i][j];
+    }
+  }
+
+  if (sum1 == sum2)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
 int main()
 {
 
