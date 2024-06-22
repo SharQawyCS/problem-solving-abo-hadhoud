4b21cee114725106805bd3945cc5f9721afc2e58
SharQawyCS <sharqawycs@gmail.com>
2024-06-22 07:59:30 +0300

    #

--- problems-from-1-to-20/tempCodeRunnerFile.cpp
+++ problems-from-1-to-20/tempCodeRunnerFile.cpp
@@ -1,43 +1,21 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
-
-int randomNumber(int From, int To)
-{ // Function to generate a random number
-  int randNum = rand() % (To - From + 1) + From;
-  return randNum;
-}
-
-void fillMatrix(int arr[3][3])
+bool isMatricesEqual(int matrix1[3][3], int matrix2[3][3], short rows, short cols)
 {
-  for (int i = 0; i < 3; i++)
+  int sum1 = 0, sum2 = 0;
+  for (int i = 0; i < rows; ++i)
   {
-    for (int j = 0; j < 3; j++)
+    for (int j = 0; j < cols; ++j)
     {
-      arr[i][j] = randomNumber(1, 100);
+      sum1 += matrix1[i][j];
+      sum2 += matrix2[i][j];
     }
   }
-}
 
-void printMatrix(int arr[3][3])
-{
-  for (int i = 0; i < 3; i++)
+  if (sum1 == sum2)
   {
-    for (int j = 0; j < 3; j++)
-    {
-      cout << setw(3) << arr[i][j] << "  ";
-    }
-    cout << endl;
+    return true;
   }
-}
-
-int main()
-{
-  srand((unsigned)time(NULL));
-  int matrix[3][3];
-
-  fillMatrix(matrix);
-  printMatrix(matrix);
-
-  return 0;
+  else
+  {
+    return false;
+  }
 }
